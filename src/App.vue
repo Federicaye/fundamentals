139bd021e858553ade98a1c32e456254fefb2e79
Federica Gallo <gallofederica58@gmail.com>
2024-04-20 17:44:31 +0200

    style button

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,18 @@
   <div>
     <h1>Methods</h1>
     <ArraysComponent />
-    <div class="insertNumber"><input type="number" v-model="data.numberUser"></div>
+    <div class="insertNumber"><input type="number" v-model="data.numberUser" class="inputNumber"></div>
     <div class="container">
       <div>
-        <button @click="data.actions.pushArray(data.numberUser)">push</button>
+        <button @click="data.actions.pushArray(data.numberUser)" class="button-green">push</button>
         <span>aggiungi un numero all'array</span>
       </div>
       <div>
-        <button @click="data.actions.atArray(data.numberUser)">at</button>
+        <button @click="data.actions.atArray(data.numberUser)" class="button-green">at</button>
         <span>ritorna l'elemento dell'array con quell'indice: {{ data.numberAt }}</span>
       </div>
        <div>
-        <button @click="data.actions.copyWithinArray(data.x, data.y)">copyWithin</button>
+        <button @click="data.actions.copyWithinArray(data.x, data.y)" class="button-green">copyWithin</button>
         <span>  Copy to index <input type="number" v-model="data.x"> all elements from index <input type="number" v-model="data.y"> to the end</span>
       </div> 
     </div>
@@ -47,5 +47,16 @@ h1 {
 
 .insertNumber {
   text-align: center;
+  
+}
+
+.inputNumber {
+  border: 6px solid red;
+}
+
+.button-green {
+  padding: 8px;
+  background-color: rgb(211, 235, 175);
+  border-radius: 10px;
 }
 </style>
