39c9c9f5bec6e9e5525265771ee5166700f3d69d
Federica Gallo <gallofederica58@gmail.com>
2024-04-21 15:44:29 +0200

    findIndex

--- src/data.js
+++ src/data.js
@@ -1,22 +1,22 @@
 import { reactive } from 'vue';
 export const data = reactive({
     arrayNumbers: [1, 2, 3, 4, 5, 6],
-    arrayNumbers2: [8,7,6,5,4,3],
-    
+    arrayNumbers2: [8, 7, 6, 5, 4, 3],
+
     actions: {
         pushArray(n) {
             data.arrayNumbers.push(n);
             console.log(data.arrayNumbers);
-        }, 
+        },
         atArray(n) {
-           data.numberAt = data.arrayNumbers.at(n);         
+            data.numberAt = data.arrayNumbers.at(n);
         },
         copyWithinArray(x, y) {
             data.arrayNumbers.copyWithin(x, y);
         },
         entriesArray() {
-           data.arrayIterator = data.arrayNumbers.entries();
-           console.log(data.arrayIterator);
+            data.arrayIterator = data.arrayNumbers.entries();
+            console.log(data.arrayIterator);
         },
         fillArray(x, y, z) {
             data.arrayNumbers.fill(x, y, z);
@@ -25,15 +25,18 @@ export const data = reactive({
         /* filterArray(){
             data.arrayNumbers2 = data.arrayNumbers.filter((n) =>  n < 4);
         }, */
-        filterArray(){
+        filterArray() {
             data.arrayNumbers2 = data.arrayNumbers.filter(this.greaterThan);
-        }, 
+        },
         greaterThan(n) {
             return n >= +data.variabileF;
-          }
-        
-        
-
+        },
+        findIndexArray(){
+           data.indexOf = data.arrayNumbers.findIndex(this.greaterThan);
+           if (data.indexOf === "-1"){
+            data.indexOf ="nessun valore soddisfa il criterio di ricerca"
+           };
+        },
     },
     numberUser: "10",
     numberAt: "",
@@ -44,6 +47,7 @@ export const data = reactive({
     variabileD: "",
     variabileE: "",
     variabileF: "",
+    indexOf: "",
 
 })
 /*  pushArray(array) {
